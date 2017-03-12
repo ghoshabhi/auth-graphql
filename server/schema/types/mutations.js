ee85d400174e90bc8ea455051acdd11d8a27ff61
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-11 19:21:08 -0800

    Feat: Create signup mutation

---
+++ server/schema/types/mutations.js
@@ -0,0 +1,30 @@
+const graphql = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString
+} = graphql;
+const UserType = require('../types/user_type');
+const AuthService = require('../../services/auth');
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        console.log(req);
+        return AuthService.signup({
+          email, 
+          password, 
+          req 
+        });
+      }
+    }
+  }
+});
+
+module.exports = mutation;
