d4c1b89644b4fea28a9b6cda8a7b553cfeff147e
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-11 20:35:36 -0800

    Init: Initiate navbar implementation

--- client/components/header.js
+++ client/components/header.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import query from '../queries/fetchUserQuery';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   isAuthenticated: false
+    // }
+  }
+
+  // componentDidMount() {
+  //   if (this.props.data.user){
+  //     this.setState({
+  //       isAuthenticated: true,
+  //     });
+  //   }
+  // }
+
+  renderButtons() {
+    const {
+      loading,
+      user
+    } = this.props.data;
+
+    if (loading) {
+      return <div/>;
+    }
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Singup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
+  }
+
   render() {
+    // const {
+    //   isAuthenticated
+    // } = this.state;
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">
+            {this.renderButtons()}
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
