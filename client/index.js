d5b2e367dcef7ae52f3af4b363866d6b16ceba35
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-11 20:01:08 -0800

    Feat: Add boilerplate code react-router

--- client/index.js
+++ client/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+import App from './components/app';
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id, //used to identify data by id to tell react which components to rerender
@@ -10,9 +12,10 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client} >
-      <div>
-        Auth Starter
-      </div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
