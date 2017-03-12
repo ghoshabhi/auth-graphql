7fb0c894dc3c92dfc6acdd12a94032b9db4a61c8
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-11 19:28:34 -0800

    Feat: Implement logout mutation

--- server/schema/types/mutations.js
+++ server/schema/types/mutations.js
@@ -23,6 +23,14 @@ const mutation = new GraphQLObjectType({
           req 
         });
       }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
+      }
     }
   }
 });
