c0422e28a91cdcc17272f802943051f243d86726
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-11 19:20:41 -0800

    Feat: Add mutation to Schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,8 +1,9 @@
 const graphql = require('graphql');
 const { GraphQLSchema } = graphql;
-
+const mutation = require('../schema/types/mutations');
 const RootQueryType = require('./types/root_query_type');
 
 module.exports = new GraphQLSchema({
-  query: RootQueryType
+  query: RootQueryType,
+  mutation
 });
